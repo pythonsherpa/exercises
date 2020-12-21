eb0b77817eebdbe0ae58131b7d3bfb5ada38c148
Joris <j.h.hoendervangers@gmail.com>
2020-12-21 17:44:07 +0100

    Create docstrings.py

---
+++ docstrings.py
@@ -0,0 +1,29 @@
+"""
+Add or change 'docstrings' to follow the Python conventions.
+"""
+
+
+# 1
+def square(n):
+    return n * n
+
+
+# 2
+def count_vowels(word):
+    # Return the total number of vowels
+    number_of_vowels = 0
+    for char in word.lower():
+        if char in "aeiou":
+            number_of_vowels += 1
+    return number_of_vowels
+
+
+# 3
+class Dog:
+    'A class to represent a dog.'
+
+    def __init__(self, name):
+        self.name = name
+
+    def bark(self):
+        print(f"{self.name} says whoof!")
