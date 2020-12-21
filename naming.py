9d60604c9af269ffbb7317b81edf1b6c3e777a19
Joris <j.h.hoendervangers@gmail.com>
2020-12-21 17:19:46 +0100

    Create naming.py

---
+++ naming.py
@@ -0,0 +1,38 @@
+"""
+Rename the following variables and function names
+"""
+
+# 1
+var = input("What is your first name? ")
+
+#
+lastName = "Johnson"
+
+#
+MovieTitle = "Pulp Fiction"
+
+#
+l = ["Windows", "Linux", "MacOS"]
+
+#
+actrs = ["Tom Hanks", "Brad Pitt", "Johnny Depp"]
+
+#
+list_of_fruits = ["apple", "banana", "kiwi", "orange"]
+
+#
+grades_dict = {"English": 90, "Biology": 80, "Math": 100}
+
+#
+def convTemp(x):
+    """Return temperature converted from Celsius to Fahrenheit"""
+    f = (x * 1.8) + 32
+    return f
+
+
+#
+class electricvehicle:
+
+    def __init__(self, brand, battery):
+        self.brand = brand
+        self.battery = battery
