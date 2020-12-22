10d0f950bc449ec7dfc53e76221782cceb001a99
Joris <j.h.hoendervangers@gmail.com>
2020-12-22 12:36:19 +0100

    Update pylint_exercise.py

--- pylint_exercise.py
+++ pylint_exercise.py
@@ -2,30 +2,10 @@
 This is an exercise to get familiar with Pylint. 
 
 You can install pylint with:
-
 pip install pylint
-
-Documentation: https://www.pylint.org
 
 To use pylint, run the following in the terminal/commandline:
 pylint pylint_exercise.py
-
-To get help, run the following in the terminal/commandline:
-pylint --help-msg=unused-variable
-
-To suppress warnings, run the following in the terminal/commandline:
-pylint exercise_3_pylint.py --disable=missing-function-docstring
-
-Extra challenge 1:
-Check out the plugin for PyCharm: https://github.com/leinardi/pylint-pycharm
-
-Extra challenge 2:
-Create a '.pylintrc' file with your own configuration
-pylint --generate-rcfile > .pylintrc
-
-Extra challenge 3:
-Check out Flake8: https://flake8.pycqa.org/en/latest/
-Which one do you prefer, pylint or flake8?
 """
 
 import statistics
