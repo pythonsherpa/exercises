577b8119cbdc775a69069f5186be0a1e2bb60202
Joris Hoendervangers <joris@hoendervangers.org>
2021-02-18 13:37:09 +0100

    add numbers

--- exercises/naming.py
+++ exercises/naming.py
@@ -5,32 +5,33 @@ Rename the following variables and function names
 # 1
 var = input("What is your first name? ")
 
-#
+# 2
 lastName = "Johnson"
 
-#
+# 3
 MovieTitle = "Pulp Fiction"
 
-#
+# 4
 l = ["Windows", "Linux", "MacOS"]
 
-#
+# 5
 actrs = ["Tom Hanks", "Brad Pitt", "Johnny Depp"]
 
-#
+# 6
 list_of_fruits = ["apple", "banana", "kiwi", "orange"]
 
-#
+# 7
 grades_dict = {"English": 90, "Biology": 80, "Math": 100}
 
-#
+
+# 8
 def convTemp(x):
     """Return temperature converted from Celsius to Fahrenheit"""
     f = (x * 1.8) + 32
     return f
 
 
-#
+# 9
 class electricvehicle:
 
     def __init__(self, brand, battery):
