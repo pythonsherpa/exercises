7d1c4560009b4eda6cf6c891ad6725c5104433c3
Joris Hoendervangers <joris@hoendervangers.org>
2021-06-17 16:06:41 +0200

    improved instructions

--- exercises/naming.py
+++ exercises/naming.py
@@ -2,36 +2,38 @@
 Rename the following variables and function names
 """
 
-# 1
-var = input("What is your first name? ")
 
-# 2
-lastName = "Johnson"
+def rename_variables_in_this_function():
+    # 1
+    var = input("What is your first name? ")
 
-# 3
-MovieTitle = "Pulp Fiction"
+    # 2
+    lastName = "Johnson"
 
-# 4
-l = ["Windows", "Linux", "MacOS"]
+    # 3
+    MovieTitle = "Pulp Fiction"
 
-# 5
-actrs = ["Tom Hanks", "Brad Pitt", "Johnny Depp"]
+    # 4
+    l = ["Windows", "Linux", "MacOS"]
 
-# 6
-list_of_fruits = ["apple", "banana", "kiwi", "orange"]
+    # 5
+    actrs = ["Tom Hanks", "Brad Pitt", "Johnny Depp"]
 
-# 7
-grades_dict = {"English": 90, "Biology": 80, "Math": 100}
+    # 6
+    list_of_fruits = ["apple", "banana", "kiwi", "orange"]
+
+    # 7
+    grades_dict = {"English": 90, "Biology": 80, "Math": 100}
 
 
-# 8
+# Rename this function and its variables
 def convTemp(x):
     """Return temperature converted from Celsius to Fahrenheit"""
     f = (x * 1.8) + 32
     return f
 
 
-# 9
+# Rename this class
 class electricvehicle:
 
     def __init__(self, brand, battery):
--- solutions/naming.py
+++ solutions/naming.py
@@ -2,36 +2,38 @@
 Rename the following variables and function names
 """
 
-# 1 - descriptive name
-first_name = input("What is your first name? ")
 
-# 2 - snake_case
-last_name = "Johnson"
+def rename_variables_in_this_function():
+    # 1 - descriptive name
+    first_name = input("What is your first name? ")
 
-# 3 - snake_case (could even be just "movie")
-movie_title = "Pulp Fiction"
+    # 2 - snake_case
+    last_name = "Johnson"
 
-# 4 - descriptive name
-operating_systems = ["Windows", "Linux", "MacOS"]
+    # 3 - snake_case (could even be just "movie")
+    movie_title = "Pulp Fiction"
 
-# 5 - no abbreviations
-actors = ["Tom Hanks", "Brad Pitt", "Johnny Depp"]
+    # 4 - descriptive name
+    operating_systems = ["Windows", "Linux", "MacOS"]
 
-# 6 - type can be inferred
-fruits = ["apple", "banana", "kiwi", "orange"]
+    # 5 - no abbreviations
+    actors = ["Tom Hanks", "Brad Pitt", "Johnny Depp"]
 
-# 7 - type can be inferred
-grades = {"English": 90, "Biology": 80, "Math": 100}
+    # 6 - type can be inferred
+    fruits = ["apple", "banana", "kiwi", "orange"]
+
+    # 7 - type can be inferred
+    grades = {"English": 90, "Biology": 80, "Math": 100}
 
 
-# 8 - snake_case & no abbreviation
-def convert_temperature(x):
+# Rename this function and its variables
+def convert_temperature(celsius):
     """Return temperature converted from Celsius to Fahrenheit"""
-    f = (x * 1.8) + 32
-    return f
+    fahrenheit = (celsius * 1.8) + 32
+    return fahrenheit
 
 
-# 9 - PascalCase for classes
+# Rename this class -> PascalCase
 class ElectricVehicle:
 
     def __init__(self, brand, battery):
