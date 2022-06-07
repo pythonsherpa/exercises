67415995d74da803cfc67b193f413f4216dec04a
Joris Hoendervangers <joris@pythonsherpa.com>
2022-06-07 21:56:48 +0200

    add tests

--- exercises/refactoring_arrow.py
+++ exercises/refactoring_arrow.py
@@ -2,37 +2,61 @@
 Refactor the code below. Try go get the functions' bodies
 as close to the left side as possible (only one indentation deep).
 """
-movies = {
-    "tomorrow_never_dies": {"name": "Tomorrow Never Dies", "genre": "Action"},
-    "robin_hood": {"name": "Robin Hood", "genre": "Adventure"},
-    "pulp_fiction": {"name": "Pulp Fiction", "genre": "Crime"},
+import pytest
+
+available_movies = {
+    "Tomorrow Never Dies": {"id": "tomorrow_never_dies", "quality": "IMAX", "genre": "Action"},
+    "Robin Hood": {"id": "robin_hood", "quality": "regular", "genre": "Adventure"},
+    "Pulp Fiction": {"id": "pulp_fiction", "quality": "regular", "genre": "Crime"},
 }
 
 
 def invoice(movie, tickets):
     amount = 0
-    for title in movies:
+    movie_in_available_movies = False
+    for title in available_movies:
         if title == movie:
+            movie_in_available_movies = True
             if tickets != 0:
-                if movie not in movies:
-                    raise LookupError("Movie not available")
-                else:
-                    if movies[title]["genre"] == "Action":
-                        price = 12
-                        if tickets >= 5:
-                            discount = 10
-                        else:
-                            discount = 0
+                if available_movies[movie]["quality"] == "IMAX":
+                    price = 12
+                    if tickets >= 5:
+                        discount = 10
                     else:
-                        price = 10
-                        if tickets >= 5:
-                            discount = 10
-                        else:
-                            discount = 0
+                        discount = 0
+                else:
+                    price = 10
+                    if tickets >= 5:
+                        discount = 10
+                    else:
+                        discount = 0
             else:
                 raise ValueError("Cannot calculate price for 0 tickets")
-    total_amount = amount + (tickets * price) - discount
-    return total_amount
+    if movie_in_available_movies is False:
+        raise LookupError("Movie not available")
+    else:
+        total_amount = amount + (tickets * price) - discount
+        return total_amount
 
 
-print(invoice("pulp_fiction", 6))
+# Test suite
+def test_invoice_without_discount():
+    assert invoice("Pulp Fiction", 1) == 10
+
+
+def test_invoice_with_discount():
+    assert invoice("Pulp Fiction", 6) == 50
+
+
+def test_invoice_imax_movie():
+    assert invoice("Tomorrow Never Dies", 3) == 36
+
+
+def test_invoice_0_tickets():
+    with pytest.raises(ValueError):
+        invoice("Pulp Fiction", 0)
+
+
+def test_invoice_unknown_movie():
+    with pytest.raises(LookupError):
+        invoice("Unknown Movie", 5)
--- solutions/refactoring_arrow.py
+++ solutions/refactoring_arrow.py
@@ -2,17 +2,19 @@
 Refactor the code below. Try go get the functions' bodies
 as close to the left side as possible (only one indentation deep).
 """
-movies = {
-    "tomorrow_never_dies": {"name": "Tomorrow Never Dies", "genre": "Action"},
-    "robin_hood": {"name": "Robin Hood", "genre": "Adventure"},
-    "pulp_fiction": {"name": "Pulp Fiction", "genre": "Crime"},
+import pytest
+
+available_movies = {
+    "Tomorrow Never Dies": {"id": "tomorrow_never_dies", "quality": "IMAX", "genre": "Action"},
+    "Robin Hood": {"id": "robin_hood", "quality": "regular", "genre": "Adventure"},
+    "Pulp Fiction": {"id": "pulp_fiction", "quality": "regular", "genre": "Crime"},
 }
 
 
 def invoice(movie, tickets):
     if tickets == 0:
         raise ValueError("Cannot calculate price for 0 tickets")
-    if movie not in movies:
+    if movie not in available_movies:
         raise LookupError("Movie not available")
 
     price = price_for(movie)
@@ -23,7 +25,7 @@ def invoice(movie, tickets):
 
 
 def price_for(movie):
-    if movies[movie]["genre"] == "Action":
+    if available_movies[movie]["genre"] == "Action":
         price = 12
     else:
         price = 10
@@ -38,4 +40,24 @@ def discount_for(tickets):
     return discount
 
 
-print(invoice("pulp_fiction", 6))
+# Test suite
+def test_invoice_without_discount():
+    assert invoice("Pulp Fiction", 1) == 10
+
+
+def test_invoice_with_discount():
+    assert invoice("Pulp Fiction", 6) == 50
+
+
+def test_invoice_imax_movie():
+    assert invoice("Tomorrow Never Dies", 3) == 36
+
+
+def test_invoice_0_tickets():
+    with pytest.raises(ValueError):
+        invoice("Pulp Fiction", 0)
+
+
+def test_invoice_unknown_movie():
+    with pytest.raises(LookupError):
+        invoice("Unknown Movie", 5)
